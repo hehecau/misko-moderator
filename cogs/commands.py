84a846e167ee20f285eac34cbcce08f644ec3471
hehecau <ytxteam97@gmail.com>
2025-04-01 17:11:53 +0200

    .helpme
    + Help príkaz

--- cogs/commands.py
+++ cogs/commands.py
@@ -16,11 +16,19 @@ class CommandCog(commands.Cog):
         else:
             await ctx.send("Misko nema mozgovu kapacitu na take velke cisla")
 
-
     @spamuser.error
     async def spamuser_errorHandle(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("Použitie: `?spamuser <Počet opakovaní> <@User>`")
+
+    @commands.command(name="helpme")
+    async def sendHelpMessage(self, ctx):
+        embed = discord.Embed(title="Misko bot v1.0", url="https://github.com/hehecau/misko-moderator", description="Open source discord.py based discord bot na moderáciu miska.", colour=0xffdd00)
+        embed.set_author(name="Misko bot")
+        embed.add_field(name="Príkazy", value="**?spamuser <Počet> <Uživateľ>** - Podľa počtu začne spamovať tag na uživateľa (misko nepodporuje nad 100 braincells)\n**?helpme** - Zobrazí toto menu.", inline=False)
+        embed.set_thumbnail(url="https://i.imgur.com/fm6q0oN.png")
+        embed.set_footer(text="misko moderator - vyrobene v socialistickej demokratickej republike", icon_url="https://i.imgur.com/fm6q0oN.png")
+        await ctx.send(embed=embed)
 
 
 
